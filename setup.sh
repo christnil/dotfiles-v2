18b63c6ec5bfccfa49382953db0a02efb530ccce
Christoffer Nilsson <christoffer.nilsson@svenskaspel.se>
2025-03-24 12:42:37 +0100

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Get the directory of the script (i.e., the root of your dotfiles repo)
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# tmux
+ln -sf "$DOTFILES_DIR/tmux" ~/.config/tmux
+ln -sf "$DOTFILES_DIR/tmux/tmux.conf" ~/.tmux.conf
+
+# TPM (Tmux Plugin Manager)
+TPM_DIR=~/.tmux/plugins/tpm
+if [ ! -d "$TPM_DIR" ]; then
+  echo "Installing TPM..."
+  git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
+else
+  echo "TPM already installed."
+fi
+
+# aerospace
+ln -sf "$DOTFILES_DIR/aerospace" ~/.config/aerospace
+
+# git
+ln -sf "$DOTFILES_DIR/gitconfig.symlink" ~/.gitconfig
+
+# bin (only if sudo)
+for file in "$DOTFILES_DIR/bin/"*; do
+  sudo ln -sf "$file" /usr/local/bin/$(basename "$file")
+done
